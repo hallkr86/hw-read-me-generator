586355f3c8dd509777a85e1ce9d9b1882a6cf348
Kesha Hall <k_renee10@yahoo.com>
2020-09-24 18:16:45 -0400

    added prompts

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-console.log("hello world");
+//console.log("hello world");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "Please enter the title of your readMe?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter the description of your project",
+            name: "name"   
+        },
+        {
+            type: "input",
+            message: "Please add the table of contents",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter information about your installation",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter information about your usage",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter you license",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please add a section about contributing",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter your tests here",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter your questions",
+            name: "name"
+        },
+    ])
